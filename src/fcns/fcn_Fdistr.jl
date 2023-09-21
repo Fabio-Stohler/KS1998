4a2d9c5e801b5d40baa7ad0bda13e7b4d4d40b01
Fabio Stohler <58552478+Fabio-Stohler@users.noreply.github.com>
2023-09-21 14:30:04 +0200

    Updated finding of a stationary initial distribution

--- src/fcns/fcn_Fdistr.jl
+++ src/fcns/fcn_Fdistr.jl
@@ -1,17 +1,18 @@
 # Residual of a distribution
-function F_distr(distr::Array, grid::Array, target_mean::Float64)
+function F_distr(input::Array, grid::Array, target_mean::Float64)
+    # Transforms the input into a probability distribution
+    N = Normal(0.0, 1.0)
+    distr = pdf.(N, input) ./ sum(pdf.(N, input)) 
 
     # Output for two residuals
-    residuals = zeros(3)
+    residuals = zeros(2)
 
     # Condition one
+    # residuals = sum(distr .* grid) - target_mean
     residuals[1] = sum(distr .* grid) - target_mean
 
     # Condition two
     residuals[2] = sum(distr) - 1.0
-
-    # Condition three
-    residuals[3] = sum(distr .< 0.0) + sum(distr .> 1.0)
 
     return residuals
 end
@@ -19,9 +20,13 @@ end
 
 # Finding a valid initial distribution
 function initial_distr(ngridk::Int64, nstates_id::Int64, k::Array, k_ss::Float64)
-    distr = zeros((ngridk, nstates_id))
+    input = ones((ngridk, nstates_id))
     f(x) = F_distr(x, k, k_ss)
-    sol = nlsolve(f, distr)
-    distr = sol.zero / sum(sol.zero)
+    sol = nlsolve(f, input, iterations = 400) # Mostly no more gain after 400 iterations
+    if norm(f(sol.zero)) > 10e-5
+        println("The initial distribution has an error of:")
+        println(f(sol.zero)[:])
+    end
+    distr = pdf.(Normal(), sol.zero) ./ sum(pdf.(Normal(), sol.zero))
     return distr
 end
