47de601454700300b39b5540c67ba97dcbf4f515
Fabio Stohler <58552478+Fabio-Stohler@users.noreply.github.com>
2023-09-22 15:15:01 +0200

    Deleted one option

--- src/script.jl
+++ src/script.jl
@@ -10,4 +10,4 @@ push!(LOAD_PATH, pwd())
 using KS, Revise
 
 # Solving for the aggregate law of motion
-B, km_ts, k_pred, distr, k_prime, c, ag_shock = @time solve_ALM(true, true);
+B, km_ts, k_pred, distr, k_prime_init, c_init, ag_shock = @time solve_ALM(true, true);
