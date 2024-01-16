7677aafe5f45a962c7415ee5260387644e48b64f
Fabio Stohler <58552478+Fabio-Stohler@users.noreply.github.com>
2024-01-16 21:07:24 +0100

    Cleaned up the KS1998 version

--- src/fcns/parameters.jl
+++ src/fcns/parameters.jl
@@ -19,20 +19,11 @@ end
     # Boundaries for asset grids
     k_min::Int = 0
     k_max::Int = 250
-    b_min::Int = -1.0
-    b_max::Int = 50.0
-    ω_min::Int = 0.0
-    ω_max::Int = 1.0
-    x_min::Int = 0.0
-    x_max::Int = 100.0
     km_min::Int = 30
     km_max::Int = 50
 
     # Number of respective gridspoints
     ngridk::Int = 100
-    ngridb::Int = 100
-    ngridx::Int = 100
-    ngridω::Int = 20
     ngridkm::Int = 4
     nstates_id::Int = 2          # number of states for the idiosyncratic shock
     nstates_ag::Int = 2          # number of states for the aggregate shock
@@ -48,9 +39,6 @@ end
         km::Array{Float64,1} = range(km_min, km_max, ngridkm)
         ϵ::Array{Float64,1} = range(0.0, nstates_id - 1.0)
         a::Array{Float64,1} = [1 - δ_a, 1 + δ_a]
-    b::Array{Float64,1} = exp.(range(0, stop = log(b_max - b_min + 1.0), length = ngridb)) .+ b_min .- 1.0
-    x::Array{Float64,1} = exp.(range(0, stop = log(x_max - x_min + 1.0), length = ngridx)) .+ x_min .- 1.0 
-    ω::Array{Float64,1} = exp.(range(0, stop = log(ω_max - ω_min + 1.0), length = ngridω)) .+ ω_min .- 1.0
 
     # Meshes for EGM
     mesh_k::Array{Float64} =
@@ -61,12 +49,6 @@ end
         repeat(reshape(a, (1, 1, nstates_ag, 1)), outer = [ngridk, ngridkm, 1, nstates_id])
     mesh_ϵ::Array{Float64} =
         repeat(reshape(ϵ, (1, 1, 1, nstates_id)), outer = [ngridk, ngridkm, nstates_ag, 1])
-    mesh_x::Array{Float64, 4} =
-        repeat(reshape(x, (ngridx, 1, 1, 1)), outer = [1, ngridkm, nstates_ag, nstates_id])
-    mesh_ω::Array{Float64, 5} = 
-        repeat(reshape(ω, (ngridω, 1, 1, 1, 1)), outer = [1, ngridx, ngridkm, nstates_ag, nstates_id])
-    mesh_b::Array{Float64, 5} =
-        repeat(reshape(b, (1, ngridb, 1, 1, 1)), outer = [ngridω, 1, ngridkm, nstates_ag, nstates_id])
 
     # Employment / Unemployment rates
     ur_b::Float64 = shocks_parameters()[1]
