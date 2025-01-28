0db69c77a42bb4da6c588a7354fa7e05c4551a62
Fabio Stohler <58552478+Fabio-Stohler@users.noreply.github.com>
2025-01-28 15:23:04 +0100

    Cleaned code

--- src/KS.jl
+++ src/KS.jl
@@ -95,16 +95,12 @@ function solve_ALM(plotting = false, plotting_check = false)
             reshape([B_new[1], B_new[1] + B_new[2], B_new[3], B_new[3] + B_new[4]], (2, 2))
         dif_B = norm(B_mat - B)
 
-        # if iteration % 100 == 0
         println("Iteration: ", iteration)
         println("Average capital: ", round(mean(km_ts[(npar.burn_in):end]); digits = 5))
         println("Error: ", dif_B)
         println("Error in pol. fcn.: ", dif_pol)
-        # println("Pure coefficients: ", B_new)
-        # println("Coefficients: ", B_mat)
         println("R²: ", round(r2(ols); digits = 5))
         println(" ")
-        # end
 
         """
         To ensure that the initial capital distribution comes from the ergodic set,
@@ -114,195 +110,33 @@ function solve_ALM(plotting = false, plotting_check = false)
         When the solution is sufficiently accurate, we stop the updating and hold the distribution
         k_cross fixed for the remaining iterations
         """
-        # if dif_B > (criter_B*100)
-        distr = copy(distr1) # replace cross-sectional capital distribution
-        # end
+
+        # replace cross-sectional capital distribution
+        distr = copy(distr1)
 
         # Plotting the results
         if plotting_check
             # Generate time series of capital
-            k_alm = zeros(npar.T)
-            k_alm[1] = km_ts[1]
-            for t in range(2; length = npar.T - 1)
-                k_alm[t] = exp(
-                    B[npar.ag_shock[t - 1], 1] .+
-                    B[npar.ag_shock[t - 1], 2] * log(km_ts[t - 1]),
-                )
-            end
-            plot(km_ts[(npar.burn_in):(end - 1)]; label = "Model")
-            plot!(k_alm[(npar.burn_in):(end - 1)]; label = "ALM")
-            display(plot!(; title = "Capital series", xlabel = "Time", ylabel = "Capital"))
+            k_alm = simulate_data(km_ts, B, npar)
+
+            # plot the results
+            plot_simulation(km_ts, k_alm)
         end
         B = B_mat .* npar.update_B .+ B .* (1 .- npar.update_B) # update the vector of ALM coefficients
         iteration += 1
     end
 
     # Generate time series of capital
-    k_alm = zeros(npar.T)
-    k_alm[1] = km_ts[1]
-    for t in range(2; length = npar.T - 1)
-        k_alm[t] = exp(
-            B[npar.ag_shock[t - 1], 1] .+ B[npar.ag_shock[t - 1], 2] * log(km_ts[t - 1]),
-        )
-    end
+    k_alm = simulate_data(km_ts, B, npar)
+
     if plotting
         # Plotting the results
-        plot(km_ts[(npar.burn_in):(end - 1)]; label = "Model")
-        plot!(k_alm[(npar.burn_in):(end - 1)]; label = "ALM")
-        display(plot!(; title = "Capital series", xlabel = "Time", ylabel = "Capital"))
-
-        println(
-            "The norm between the two series is: ",
-            norm(km_ts[(npar.burn_in):end] .- k_alm[(npar.burn_in):end]),
-        )
+        plot_simulation(km_ts, k_alm, "../bld/figures/beta/")
     end
-    println("The coefficients are:")
-    println(B[1, 1], " ", B[1, 2])
-    println(B[2, 1], " ", B[2, 2])
-
-    return B,
-    km_ts,
-    k_alm,
-    distr,
-    reshape(k1, (npar.ngridk, npar.ngridkm, npar.nstates_ag, npar.nstates_id)),
-    reshape(c, (npar.ngridk, npar.ngridkm, npar.nstates_ag, npar.nstates_id)),
-    npar.ag_shock,
-    mpar,
-    npar
-end
-
-function solve_ALM_Delta(plotting = false, plotting_check = false)
-    # generate structures
-    mpar = ModelParameters()
-    npar = NumericalParametersDelta()
-
-    # Initial guess for the ALM coefficients
-    B = zeros(npar.nstates_ag, 2)
-    B[:, 1] .= 0.0
-    B[:, 2] .= 1.0
-
-    # Inputs to solve_HH
-    L = repeat(
-        reshape([npar.er_b, npar.er_g], (1, 1, 1, npar.nstates_ag));
-        outer = [npar.ngridk, npar.ngridkm, npar.nstates_id, 1],
-    ) # Mesh over employment in states
-    r_t = 1.0 .+ KS.interest(npar.mesh_km, 1.0, L .* mpar.l_bar, npar.mesh_δ, mpar)
-    w_t = KS.wage(npar.mesh_km, ones(size(npar.mesh_k)), L .* mpar.l_bar, mpar)
-
-    # Defining income
-    inc::Array = [
-        w_t .* npar.mesh_ϵ .* mpar.l_bar .+ mpar.μ .* (w_t .* (1 .- npar.mesh_ϵ)) .-
-        mpar.μ .* (w_t .* (1 .- L) ./ L) .* npar.mesh_ϵ,
-        r_t .* npar.mesh_k,
-    ]
-
-    # Initial guess for the policy function
-    km_ts = zeros(npar.T)
-    c = similar(r_t)
-    k0 = similar(r_t)
-    k1 = similar(r_t)
-
-    # Initial guess for the cross-sectional distribution
-    distr = npar.distr_init
-
-    """
-    Main loop
-    Solve for HH problem given ALM
-    Generate time series km_ts given policy function
-    Run regression and update ALM
-    Iterate until convergence
-    """
-    iteration = 1
-    dif_B = 10^10
-    while dif_B > npar.ϵ_B && iteration < npar.iter_max
-        # Solve for HH policy functions at a given law of motion
-        c, k1 = solve_HH(B, inc, r_t, npar, mpar)
-
-        # Save difference in policy functions
-        dif_pol = norm(k1 - k0)
-        k0 = copy(k1)
-
-        # Generate time series and cross section of capital
-        km_ts, distr1 = aggregate_st(distr, k1, npar.ag_shock, npar)
-        """
-        run regression: log(km') = B[j,1]+B[j,2]log(km) for aggregate state
-        """
-        x = log.(km_ts[(npar.burn_in):(end - 1)])[:]
-        X = hcat(
-            ones(length(x)),
-            (npar.ag_shock .- 1)[(npar.burn_in):(end - 1)],
-            x,
-            (npar.ag_shock .- 1)[(npar.burn_in):(end - 1)] .* x,
-        )
-        y = log.(km_ts[(npar.burn_in + 1):end])[:]
-        ols = lm(X, y)
-        B_new = coef(ols)
-        # B_new = inv(X'*X)*(X'*y)
-        B_mat =
-            reshape([B_new[1], B_new[1] + B_new[2], B_new[3], B_new[3] + B_new[4]], (2, 2))
-        dif_B = norm(B_mat - B)
-
-        # if iteration % 100 == 0
-        println("Iteration: ", iteration)
-        println("Average capital: ", round(mean(km_ts[(npar.burn_in):end]); digits = 5))
-        println("Error: ", dif_B)
-        println("Error in pol. fcn.: ", dif_pol)
-        # println("Pure coefficients: ", B_new)
-        # println("Coefficients: ", B_mat)
-        println("R²: ", round(r2(ols); digits = 5))
-        println(" ")
-        # end
-
-        """
-        To ensure that the initial capital distribution comes from the ergodic set,
-        we use the terminal distribution of the current iteration as the initial distribution for
-        subsequent iterations.
-
-        When the solution is sufficiently accurate, we stop the updating and hold the distribution
-        k_cross fixed for the remaining iterations
-        """
-        # if dif_B > (criter_B*100)
-        distr = copy(distr1) # replace cross-sectional capital distribution
-        # end
-
-        # Plotting the results
-        if plotting_check
-            # Generate time series of capital
-            k_alm = zeros(npar.T)
-            k_alm[1] = km_ts[1]
-            for t in range(2; length = npar.T - 1)
-                k_alm[t] = exp(
-                    B[npar.ag_shock[t - 1], 1] .+
-                    B[npar.ag_shock[t - 1], 2] * log(km_ts[t - 1]),
-                )
-            end
-            plot(km_ts[(npar.burn_in):(end - 1)]; label = "Model")
-            plot!(k_alm[(npar.burn_in):(end - 1)]; label = "ALM")
-            display(plot!(; title = "Capital series", xlabel = "Time", ylabel = "Capital"))
-        end
-        B = B_mat .* npar.update_B .+ B .* (1 .- npar.update_B) # update the vector of ALM coefficients
-        iteration += 1
-    end
-
-    # Generate time series of capital
-    k_alm = zeros(npar.T)
-    k_alm[1] = km_ts[1]
-    for t in range(2; length = npar.T - 1)
-        k_alm[t] = exp(
-            B[npar.ag_shock[t - 1], 1] .+ B[npar.ag_shock[t - 1], 2] * log(km_ts[t - 1]),
-        )
-    end
-    if plotting
-        # Plotting the results
-        plot(km_ts[(npar.burn_in):(end - 1)]; label = "Model")
-        plot!(k_alm[(npar.burn_in):(end - 1)]; label = "ALM")
-        display(plot!(; title = "Capital series", xlabel = "Time", ylabel = "Capital"))
-
-        println(
-            "The norm between the two series is: ",
-            norm(km_ts[(npar.burn_in):end] .- k_alm[(npar.burn_in):end]),
-        )
-    end
+    println(
+        "The norm between the two series is: ",
+        norm(km_ts[(npar.burn_in):end] .- k_alm[(npar.burn_in):end]),
+    )
     println("The coefficients are:")
     println(B[1, 1], " ", B[1, 2])
     println(B[2, 1], " ", B[2, 2])
@@ -389,16 +223,13 @@ function solve_ALM_Beta(plotting = false, plotting_check = false)
             reshape([B_new[1], B_new[1] + B_new[2], B_new[3], B_new[3] + B_new[4]], (2, 2))
         dif_B = norm(B_mat - B)
 
-        # if iteration % 100 == 0
+        # Print statements
         println("Iteration: ", iteration)
         println("Average capital: ", round(mean(km_ts[(npar.burn_in):end]); digits = 5))
         println("Error: ", dif_B)
         println("Error in pol. fcn.: ", dif_pol)
-        # println("Pure coefficients: ", B_new)
-        # println("Coefficients: ", B_mat)
         println("R²: ", round(r2(ols); digits = 5))
         println(" ")
-        # end
 
         """
         To ensure that the initial capital distribution comes from the ergodic set,
@@ -408,48 +239,162 @@ function solve_ALM_Beta(plotting = false, plotting_check = false)
         When the solution is sufficiently accurate, we stop the updating and hold the distribution
         k_cross fixed for the remaining iterations
         """
-        # if dif_B > (criter_B*100)
-        distr = copy(distr1) # replace cross-sectional capital distribution
-        # end
+
+        # replace cross-sectional capital distribution
+        distr = copy(distr1)
 
         # Plotting the results
         if plotting_check
             # Generate time series of capital
-            k_alm = zeros(npar.T)
-            k_alm[1] = km_ts[1]
-            for t in range(2; length = npar.T - 1)
-                k_alm[t] = exp(
-                    B[npar.ag_shock[t - 1], 1] .+
-                    B[npar.ag_shock[t - 1], 2] * log(km_ts[t - 1]),
-                )
-            end
-            plot(km_ts[(npar.burn_in):(end - 1)]; label = "Model")
-            plot!(k_alm[(npar.burn_in):(end - 1)]; label = "ALM")
-            display(plot!(; title = "Capital series", xlabel = "Time", ylabel = "Capital"))
+            k_alm = simulate_data(km_ts, B, npar)
+
+            # plot the results
+            plot_simulation(km_ts, k_alm)
         end
         B = B_mat .* npar.update_B .+ B .* (1 .- npar.update_B) # update the vector of ALM coefficients
         iteration += 1
     end
 
     # Generate time series of capital
-    k_alm = zeros(npar.T)
-    k_alm[1] = km_ts[1]
-    for t in range(2; length = npar.T - 1)
-        k_alm[t] = exp(
-            B[npar.ag_shock[t - 1], 1] .+ B[npar.ag_shock[t - 1], 2] * log(km_ts[t - 1]),
-        )
-    end
+    k_alm = simulate_data(km_ts, B, npar)
+
     if plotting
         # Plotting the results
-        plot(km_ts[(npar.burn_in):(end - 1)]; label = "Model")
-        plot!(k_alm[(npar.burn_in):(end - 1)]; label = "ALM")
-        display(plot!(; title = "Capital series", xlabel = "Time", ylabel = "Capital"))
-
-        println(
-            "The norm between the two series is: ",
-            norm(km_ts[(npar.burn_in):end] .- k_alm[(npar.burn_in):end]),
-        )
+        plot_simulation(km_ts, k_alm, "../bld/figures/beta/")
     end
+    println(
+        "The norm between the two series is: ",
+        norm(km_ts[(npar.burn_in):end] .- k_alm[(npar.burn_in):end]),
+    )
+    println("The coefficients are:")
+    println(B[1, 1], " ", B[1, 2])
+    println(B[2, 1], " ", B[2, 2])
+
+    return B,
+    km_ts,
+    k_alm,
+    distr,
+    reshape(k1, (npar.ngridk, npar.ngridkm, npar.nstates_ag, npar.nstates_id)),
+    reshape(c, (npar.ngridk, npar.ngridkm, npar.nstates_ag, npar.nstates_id)),
+    npar.ag_shock,
+    mpar,
+    npar
+end
+
+function solve_ALM_Delta(plotting = false, plotting_check = false)
+    # generate structures
+    mpar = ModelParameters()
+    npar = NumericalParametersDelta()
+
+    # Initial guess for the ALM coefficients
+    B = zeros(npar.nstates_ag, 2)
+    B[:, 1] .= 0.0
+    B[:, 2] .= 1.0
+
+    # Inputs to solve_HH
+    L = repeat(
+        reshape([npar.er_b, npar.er_g], (1, 1, 1, npar.nstates_ag));
+        outer = [npar.ngridk, npar.ngridkm, npar.nstates_id, 1],
+    ) # Mesh over employment in states
+    r_t = 1.0 .+ KS.interest(npar.mesh_km, 1.0, L .* mpar.l_bar, npar.mesh_δ, mpar)
+    w_t = KS.wage(npar.mesh_km, ones(size(npar.mesh_k)), L .* mpar.l_bar, mpar)
+
+    # Defining income
+    inc::Array = [
+        w_t .* npar.mesh_ϵ .* mpar.l_bar .+ mpar.μ .* (w_t .* (1 .- npar.mesh_ϵ)) .-
+        mpar.μ .* (w_t .* (1 .- L) ./ L) .* npar.mesh_ϵ,
+        r_t .* npar.mesh_k,
+    ]
+
+    # Initial guess for the policy function
+    km_ts = zeros(npar.T)
+    c = similar(r_t)
+    k0 = similar(r_t)
+    k1 = similar(r_t)
+
+    # Initial guess for the cross-sectional distribution
+    distr = npar.distr_init
+
+    """
+    Main loop
+    Solve for HH problem given ALM
+    Generate time series km_ts given policy function
+    Run regression and update ALM
+    Iterate until convergence
+    """
+    iteration = 1
+    dif_B = 10^10
+    while dif_B > npar.ϵ_B && iteration < npar.iter_max
+        # Solve for HH policy functions at a given law of motion
+        c, k1 = solve_HH(B, inc, r_t, npar, mpar)
+
+        # Save difference in policy functions
+        dif_pol = norm(k1 - k0)
+        k0 = copy(k1)
+
+        # Generate time series and cross section of capital
+        km_ts, distr1 = aggregate_st(distr, k1, npar.ag_shock, npar)
+        """
+        run regression: log(km') = B[j,1]+B[j,2]log(km) for aggregate state
+        """
+        x = log.(km_ts[(npar.burn_in):(end - 1)])[:]
+        X = hcat(
+            ones(length(x)),
+            (npar.ag_shock .- 1)[(npar.burn_in):(end - 1)],
+            x,
+            (npar.ag_shock .- 1)[(npar.burn_in):(end - 1)] .* x,
+        )
+        y = log.(km_ts[(npar.burn_in + 1):end])[:]
+        ols = lm(X, y)
+        B_new = coef(ols)
+        # B_new = inv(X'*X)*(X'*y)
+        B_mat =
+            reshape([B_new[1], B_new[1] + B_new[2], B_new[3], B_new[3] + B_new[4]], (2, 2))
+        dif_B = norm(B_mat - B)
+
+        # Print statements
+        println("Iteration: ", iteration)
+        println("Average capital: ", round(mean(km_ts[(npar.burn_in):end]); digits = 5))
+        println("Error: ", dif_B)
+        println("Error in pol. fcn.: ", dif_pol)
+        println("R²: ", round(r2(ols); digits = 5))
+        println(" ")
+
+        """
+        To ensure that the initial capital distribution comes from the ergodic set,
+        we use the terminal distribution of the current iteration as the initial distribution for
+        subsequent iterations.
+
+        When the solution is sufficiently accurate, we stop the updating and hold the distribution
+        k_cross fixed for the remaining iterations
+        """
+
+        # replace cross-sectional capital distribution
+        distr = copy(distr1)
+
+        # Plotting the results
+        if plotting_check
+            # Generate time series of capital
+            k_alm = simulate_data(km_ts, B, npar)
+
+            # plot the results
+            plot_simulation(km_ts, k_alm)
+        end
+        B = B_mat .* npar.update_B .+ B .* (1 .- npar.update_B) # update the vector of ALM coefficients
+        iteration += 1
+    end
+
+    # Generate time series of capital
+    k_alm = simulate_data(km_ts, B, npar)
+
+    if plotting
+        # Plotting the results
+        plot_simulation(km_ts, k_alm, "../bld/figures/beta/")
+    end
+    println(
+        "The norm between the two series is: ",
+        norm(km_ts[(npar.burn_in):end] .- k_alm[(npar.burn_in):end]),
+    )
     println("The coefficients are:")
     println(B[1, 1], " ", B[1, 2])
     println(B[2, 1], " ", B[2, 2])
@@ -559,16 +504,13 @@ function solve_ALM_All(plotting = false, plotting_check = false)
         )
         dif_B = norm(B_mat - B)
 
-        # if iteration % 100 == 0
+        # Print statements
         println("Iteration: ", iteration)
         println("Average capital: ", round(mean(km_ts[(npar.burn_in):end]); digits = 5))
         println("Error: ", dif_B)
         println("Error in pol. fcn.: ", dif_pol)
-        # println("Pure coefficients: ", B_new)
-        # println("Coefficients: ", B_mat)
         println("R²: ", round(r2(ols); digits = 5))
         println(" ")
-        # end
 
         """
         To ensure that the initial capital distribution comes from the ergodic set,
@@ -578,48 +520,33 @@ function solve_ALM_All(plotting = false, plotting_check = false)
         When the solution is sufficiently accurate, we stop the updating and hold the distribution
         k_cross fixed for the remaining iterations
         """
-        # if dif_B > (criter_B*100)
-        distr = copy(distr1) # replace cross-sectional capital distribution
-        # end
+
+        # replace cross-sectional capital distribution
+        distr = copy(distr1)
 
         # Plotting the results
         if plotting_check
             # Generate time series of capital
-            k_alm = zeros(npar.T)
-            k_alm[1] = km_ts[1]
-            for t in range(2; length = npar.T - 1)
-                k_alm[t] = exp(
-                    B[npar.ag_shock[t - 1], 1] .+
-                    B[npar.ag_shock[t - 1], 2] * log(km_ts[t - 1]),
-                )
-            end
-            plot(km_ts[(npar.burn_in):(end - 1)]; label = "Model")
-            plot!(k_alm[(npar.burn_in):(end - 1)]; label = "ALM")
-            display(plot!(; title = "Capital series", xlabel = "Time", ylabel = "Capital"))
+            k_alm = simulate_data(km_ts, B, npar)
+
+            # plot the results
+            plot_simulation(km_ts, k_alm)
         end
         B = B_mat .* npar.update_B .+ B .* (1 .- npar.update_B) # update the vector of ALM coefficients
         iteration += 1
     end
 
     # Generate time series of capital
-    k_alm = zeros(npar.T)
-    k_alm[1] = km_ts[1]
-    for t in range(2; length = npar.T - 1)
-        k_alm[t] = exp(
-            B[npar.ag_shock[t - 1], 1] .+ B[npar.ag_shock[t - 1], 2] * log(km_ts[t - 1]),
-        )
-    end
+    k_alm = simulate_data(km_ts, B, npar)
+
     if plotting
         # Plotting the results
-        plot(km_ts[(npar.burn_in):(end - 1)]; label = "Model")
-        plot!(k_alm[(npar.burn_in):(end - 1)]; label = "ALM")
-        display(plot!(; title = "Capital series", xlabel = "Time", ylabel = "Capital"))
-
-        println(
-            "The norm between the two series is: ",
-            norm(km_ts[(npar.burn_in):end] .- k_alm[(npar.burn_in):end]),
-        )
+        plot_simulation(km_ts, k_alm, "../bld/figures/beta/")
     end
+    println(
+        "The norm between the two series is: ",
+        norm(km_ts[(npar.burn_in):end] .- k_alm[(npar.burn_in):end]),
+    )
     println("The coefficients are:")
     println(B[1, 1], " ", B[1, 2])
     println(B[2, 1], " ", B[2, 2])
